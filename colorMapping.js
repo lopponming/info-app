ffcfeb5e055d9f4dc0d439609b8a15b8755573de
lopponming <miranda.klockars@gmail.com>
2023-05-23 10:24:36 +0300

    started working on color mapping

---
+++ colorMapping.js
@@ -0,0 +1,10 @@
+//color coding/mapping the courses (basic and free-choice)
+
+const colorMapping = {
+    'media channels and platforms': 'blue',
+    'media work methods': 'green',
+    // Add more mappings here
+};
+
+// Export the colorMapping object
+export default colorMapping;
